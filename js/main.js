5907afde2b417eee05ad195c1a9bd4318ade39ea
Biljana Ljubojevic <biljana.ljubojevic@etondigital.com>
2018-08-15 13:39:30 +0200

    First commit.

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {
+  beforeAfterSlider();
+  mobileMenu();
+  slickSlider();
+});
+
+// //Mobile menu
+function mobileMenu () {
+  var btnMenu = $('.header__inner').find('#nav-icon');
+
+  btnMenu.on('click', function () {
+    $(this).toggleClass('open');
+    $('.menu').toggleClass('open');
+  })
+}
+
+//Transitionss shapes
+function beforeAfterSlider () {
+
+  var black = $('.slider__black');
+  var sliderArrow = $('.slider__black').before();
+
+  $('.slider').on('mousemove touchmove',function(e){
+    var offX  = (e.offsetX || e.clientX - black.offset().left);
+    black.width(offX);
+  });
+
+   sliderArrow.on('click', function () {
+    
+  })
+}
+
+function slickSlider () {
+  $('.t-slider').slick();
+  console.log('hghgfhgfhgfhgf');
+}
+
+
+
+
+
