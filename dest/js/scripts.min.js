ae64f5a32eef889c18d30a5cd31377eee94b7018
Biljana Ljubojevic <biljana.ljubojevic@etondigital.com>
2018-08-30 14:57:54 +0200

    Navigation style responsive, and corrections.

--- dest/js/scripts.min.js
+++ dest/js/scripts.min.js
@@ -1,13 +1,14 @@
 $( document ).ready(function() {
-  beforeAfterSlider();
-  mobileMenu();
-  slickSlider();
-  formValidation();
+  mobileMenu ();
+  iconSearch ();
+  isSmallDevice ()
+  slickSlider ();
+  formValidation ();
 });
 
-// //Mobile menu
+/* Mobile menu */
 function mobileMenu () {
-  var btnMenu = $('.header__inner').find('#nav-icon');
+  var btnMenu = $('.header__inner').find('.icon-menu');
 
   btnMenu.on('click', function () {
     $(this).toggleClass('open');
@@ -15,22 +16,28 @@ function mobileMenu () {
   })
 }
 
-//Transitionss shapes
-function beforeAfterSlider () {
+/* Icon search animation */
+function iconSearch () {
+  var search = $('.icon-search');
 
-  var black = $('.slider__black');
-  var sliderArrow = $('.slider__black').before();
-
-  $('.slider').on('mousemove touchmove',function(e){
-    var offX  = (e.offsetX || e.clientX - black.offset().left);
-    black.width(offX);
-  });
-
-   sliderArrow.on('click', function () {
-    
+  search.on('click', function () {
+    $('.f-search__control').toggleClass('extended');
   })
 }
 
+/* Window width on small devices */
+function isSmallDevice () {
+  var width = $(window).width();
+
+  if (width <= 768) {
+    $('body').addClass('s-device')
+  }
+  else {
+    $('body').addClass('b-device')
+  }
+}
+
+/* Slick sliders */
 function slickSlider () {
   $('.t-slider').slick({
     dots: true
@@ -64,7 +71,7 @@ function slickSlider () {
   });
 
 
-  $('.b-slider').on('afterChange', function() {
+  $('.b-slider').on('afterChange', function () {
     var currIndex = $(this).find('.slick-current').attr('data-slick-index');
     var navSlide = $('.b-slider__nav').find('.slick-slide');
 
@@ -79,12 +86,10 @@ function slickSlider () {
 }
 
 /* FormValidation */
-function formValidation() {
-
+function formValidation () {
   var input = $('.f-contact__controls');
- // var message = $('.f-contact__controls');
 
-  input.on('keyup', function() {
+  input.on('keyup', function () {
     if($(this).val()) {
       $(this).addClass('filed');
     } else {
@@ -93,8 +98,3 @@ function formValidation() {
     console.log('it work');
   });
 }
-
-
-
-
-
